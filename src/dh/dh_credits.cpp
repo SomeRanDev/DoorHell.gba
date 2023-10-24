68583283a5babfc4b9a5c0a750ede125cb3089f7
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-10-24 19:48:06 -0400

    Add sfx credits

--- src/dh/dh_credits.cpp
+++ src/dh/dh_credits.cpp
@@ -6,10 +6,11 @@ namespace
 {
     constexpr bn::string_view code_credits[] = { "SomeRanDev (Robert Borghese)" };
     constexpr bn::string_view graphics_credits[] = { "SomeRanDev (Robert Borghese)" };
+	constexpr bn::string_view sfx_credits[] = { "SomeRanDev (Robert Borghese)", "Amada44" };
 }
 
 MJ_GAME_LIST_ADD(dh::game)
 MJ_GAME_LIST_ADD_CODE_CREDITS(code_credits)
 MJ_GAME_LIST_ADD_GRAPHICS_CREDITS(graphics_credits)
 // MJ_GAME_LIST_ADD_MUSIC_CREDITS(music_credits)
-// MJ_GAME_LIST_ADD_SFX_CREDITS(sfx_credits)
+MJ_GAME_LIST_ADD_SFX_CREDITS(sfx_credits)
